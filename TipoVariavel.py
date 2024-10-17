481d1a81e4b92818df0bea1e7fe9d9f75ecad297
Letícia da Silva Campos <scleticiaa@gmail.com>
2024-10-17 20:36:06 -0300

    Add files via upload

---
+++ TipoVariavel.py
@@ -0,0 +1,8 @@
+codigo = 10
+salario = 1500.00
+nome = 'Leticia'
+situacao = True
+
+tipo = type (salario)
+
+print("Codigo:", codigo, "Salario:", salario, "Nome:", nome, "Situacao:", situacao, "Tipo da variavel 'Salario':", tipo)
